423c60c8ffcfc8e73ae513ff0c9df994471ef295
om-bhadange <155983830+om-bhadange@users.noreply.github.com>
2024-12-30 16:29:00 +0530

    name.java

--- name.java
+++ name.java
@@ -1,6 +1,6 @@
-public class hie { 
+public class PrintName {
     public static void main(String[] args) {
-        
-        System.out.println("Hello Om Bhadange!");
-    } 
+       
+        System.out.println("Om Bhadange");
+    }
 }
